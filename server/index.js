029aeead91c2fad8238452ea24c3fa9052ccaf99
The_Hufaaz <ufasabdulmutolib@gmail.com>
2023-04-04 16:25:43 +0100

    connected server to mongodb

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 const PORT = process.env.PORT|| 5000;
-
+const CONNECTION_URL = "mongodb+srv://abdul:KeZlHiFbVA5gLS5e@cluster0.ummun8r.mongodb.net/test";
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
