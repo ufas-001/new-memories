5a817311ecfeef3b9898f8d5b1506559cc5587ef
The_Hufaaz <ufasabdulmutolib@gmail.com>
2023-04-05 21:59:52 +0100

    deploy to ec2 instance

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 6000;
 // Connection url
 const CONNECTION_URL = "mongodb+srv://abdul:KeZlHiFbVA5gLS5e@cluster0.ummun8r.mongodb.net/test";
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
